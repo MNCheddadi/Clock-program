c99cdebe58e10ba904256419ce368a78e762edc8
MNCheddadi <m.n.chedadi@gmail.com>
2025-06-29 17:59:37 +0100

    Added title to the window and optimized update function

--- clock.py
+++ clock.py
@@ -2,15 +2,14 @@ from tkinter import *
 from time import *
 
 def update():
-    time_string = strftime("%I:%M:%S %p")
-    time_label.config(text=time_string)
-    day_string = strftime("%A")
-    day_label.config(text=day_string)
-    date_string = strftime("%B %d, %Y")
-    date_label.config(text=date_string)
+    time_label.config(text=strftime("%I:%M:%S %p"))
+    day_label.config(text=strftime("%A"))
+    date_label.config(text=strftime("%B %d, %Y"))
     window.after(1000, update)
 
 window = Tk()
+
+window.title("Digital Clock")
 
 time_label = Label(window, font=("Arial", 50), fg="#00FF00", bg="black")
 time_label.pack()
